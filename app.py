c17523be7c1b660c01e168c6933aabe05e78cf5a
Mohamad <hyper11creeper@gmail.com>
2023-02-01 21:31:03 +0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, send_from_directory, request, Response
+import requests as r
+import base64
+import json
+app = Flask(__name__)
+
+data = {}
+refresh_token = ""
+# Resources
+@app.route("/")
+def index():
+    return send_from_directory("", "index.html")
+@app.route("/index.js")
+def indexjs():
+    return send_from_directory("", "index.js")
+@app.route("/spotifyPlayer.js")
+def playerjs():
+    return send_from_directory("", "spotifyPlayer.js")
+
+@app.route("/images/Play.svg")
+def playsvg():
+    return send_from_directory("images", "Play.svg")
+
+@app.route("/images/Pause.svg")
+def pausesvg():
+    return send_from_directory("images", "Pause.svg")
+
+@app.route("/images/Next.svg")
+def nextsvg():
+    return send_from_directory("images", "Next.svg")
+
+@app.route("/images/Previous.svg")
+def prevsvg():
+    return send_from_directory("images", "Previous.svg")
+
+# Spotfy Auth
+@app.route("/login/auth")
+def spotifyauth():
+    return json.dumps(data)
+        
+        
+@app.route("/login/auth/token")
+def spotifygetauthtoken():
+    headers = request.headers
+    urlheaders = request.args
+    try:
+        # WARNING: The redirect_url arg is just used as a security measure, it does not actually redirect
+        resp = r.post("https://accounts.spotify.com/api/token?code=" + urlheaders["code"] + "&grant_type=authorization_code&redirect_uri=http://localhost:5000/login/auth/token", headers={"Content-Type": "application/x-www-form-urlencoded", "Authorization": "Basic " + base64.b64encode(b"77e587a9a2644f0eb40fa597371de715:d80711ecb59b4d9a8b7fd42fb1f72606").decode()})
+        parsed_data = json.loads(resp.text)
+        resp2 = r.get("https://api.spotify.com/v1/me",headers={"Authorization": "Bearer " + parsed_data["access_token"]})
+        global data
+        data = json.loads(resp2.text)
+        data["token"] = parsed_data["access_token"]
+        global refresh_token
+        refresh_token = parsed_data["refresh_token"]
+        return json.dumps(data) + "<br><br><BR>" + resp.text + "<a href='/'>Click to continue</a>"
+    except:
+        resp = r.post("https://accounts.spotify.com/api/token?code=" + refresh_token + "&grant_type=refresh_token&redirect_uri=http://localhost:5000/login/auth/token", headers={"Content-Type": "application/x-www-form-urlencoded", "Authorization": "Basic " + base64.b64encode(b"77e587a9a2644f0eb40fa597371de715:d80711ecb59b4d9a8b7fd42fb1f72606").decode()})
+        parsed_data = json.loads(resp.text)
+        resp2 = r.get("https://api.spotify.com/v1/me",headers={"Authorization": "Bearer " + parsed_data["access_token"]})
+        # global data
+        data = json.loads(resp2.text)
+        data["token"] = parsed_data["access_token"]
+        # global refresh_token
+        refresh_token = parsed_data["refresh_token"]
+        spotifygetauthtoken()
+    # try:
+    #     print("placeholder")
+    #     # return "<script>location.href = 'https://accounts.spotify.com/api/token?code=" + urlheaders["code"] + "&redirect_url=http://localhost:5000/login/auth&grant_type=authorization_code'</script>"
+    # except Exception as e: 
+    #     print(e)
+    #     return "Error Occured<br><br>" + str(e)
+
+@app.route("/login")
+def spotifyauthstart():
+    scopes = "user-read-private user-read-email playlist-read-private playlist-read-collaborative".replace(" ", "%20")
+    return "<script>location.href = 'https://accounts.spotify.com/authorize?response_type=code&client_id=77e587a9a2644f0eb40fa597371de715&scope=" + scopes + "&redirect_uri=http://localhost:5000/login/auth/token';</script>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ index.js
@@ -0,0 +1,75 @@
+function togglePlayback() {
+    if(document.getElementById("pausePlayButton").children[0].src == location.href.replace("/index.html", "") + "images/Play.svg") {
+        document.getElementById("pausePlayButton").children[0].src = "/images/Pause.svg";
+    }
+    else {
+        document.getElementById("pausePlayButton").children[0].src = "/images/Play.svg";
+    }
+}
+var SpotifyUserData;
+fetch('/login/auth')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    SpotifyUserData = data;
+    document.getElementById("pfpimg").src = SpotifyUserData["images"][0]["url"];
+    document.getElementById("pfpimg").title = SpotifyUserData["display_name"] + " | " + SpotifyUserData["email"];
+      fetch('https://api.spotify.com/v1/users/' + SpotifyUserData["id"] + "/playlists", {method: "GET",headers: new Headers({'Authorization': "Bearer " + SpotifyUserData["token"]})})
+      .then(response2 => response2.json())
+      .then(data2 => {
+        console.log(data2);
+        for(var i = 0; i < data2["items"].length; i++) {
+            var img = document.createElement("img");
+            var playlistitem = document.createElement("div");
+            try {
+              img.src = data2["items"][i]["images"][0]["url"];
+            } catch (TypeError) {
+              
+            }
+            // img.style.minHeight = "64";
+            img.style.maxHeight = document.getElementById("referencer").clientHeight;
+            var playlistTitle = document.createElement("b");
+            playlistTitle.innerText = data2["items"][i]["name"];
+            playlistTitle.style.display = "inline-block";
+            playlistTitle.style.overflow = "hidden";
+            playlistTitle.style.width = "125px";
+            playlistTitle.style.marginLeft = "10px";
+            playlistTitle.style.textOverflow = "ellipsis";
+            playlistTitle.style.marginBottom = "-15px";
+            playlistTitle.style.fontSize = "20px";
+            var playlistSubTitle = document.createElement("span");
+            playlistSubTitle.innerText = data2["items"][i]["owner"]["display_name"];
+            playlistSubTitle.style.display = "inline-block";
+            playlistSubTitle.style.overflow = "hidden";
+            playlistSubTitle.style.width = "125px";
+            playlistSubTitle.style.marginLeft = "10px";
+            playlistSubTitle.style.textOverflow = "ellipsis";
+            playlistSubTitle.style.marginBottom = "-15px";
+            playlistitem.style.display = "flex";
+            playlistitem.style.alignItems = "center";
+            playlistitem.classList.add("playlistListItem");
+            playlistitem.appendChild(img);
+            var downdiv = document.createElement("div");
+            downdiv.appendChild(playlistTitle);
+            downdiv.appendChild(document.createElement("br"));
+            downdiv.appendChild(document.createElement("br"));
+            downdiv.appendChild(document.createElement("br"));
+            downdiv.appendChild(playlistSubTitle);
+            // playlistitem.appendChild(playlistTitle);
+            playlistitem.appendChild(downdiv);
+            document.getElementsByClassName("playlistListContainer")[0].appendChild(playlistitem);
+            var imggradient = document.createElement("div");
+            imggradient.style.position = "absolute";
+            imggradient.style.top = img.offsetTop + "px";
+            imggradient.style.left = img.offsetLeft + "px";
+            imggradient.style.height = img.clientHeight + "px";
+            imggradient.style.width = img.clientWidth+0 + "px";
+            imggradient.style.background = "-webkit-linear-gradient(0deg, transparent, #c36daa)";
+            imggradient.style.background = "linear-gradient(90deg, transparent, #c36daa)";
+            playlistitem.appendChild(imggradient);
+        }
+    })
+      .catch(error => console.error(error));
+
+})
+  .catch(error => console.error(error));
